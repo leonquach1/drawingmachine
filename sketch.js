ff8e65fe8ef267fbd5176d377dae7badf0c51643
LeonQuach <31291667+LeonQuach@users.noreply.github.com>
2019-10-03 11:25:34 -0700

    Video 1 updates

--- sketch.js
+++ sketch.js
@@ -1,7 +1,32 @@
 function setup() {
   createCanvas(400, 400);
+  background(220);
+  strokeWeight(5);
 }
 
 function draw() {
-  background(220);
+
+  if (mouseIsPressed) {
+    stroke(map(mouseX, 0, 600, 0, 255, true))
+    line(width - mouseX, height - mouseY, width -  pmouseX, height - pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  } else {
+
+  }
 }
+
+ function keyTyped(){
+    // console.log(`key ${key} is being typed`);
+    // console.log("key " + key + "is being typed");
+   if (key === 's'){
+     //save drawn image
+     saveCanvas('fileName', 'png');
+   }
+
+   if (key === 'e'){
+     background(220);
+
+   }
+
+   return false;
+ }
